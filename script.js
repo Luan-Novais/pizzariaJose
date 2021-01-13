c19e70f4cbd47fc83527cfbad44a142d66a4505e
Luan-Novais <luan.novaiss@outlook.com>
2021-01-13 08:32:13 -0300

    Adc modal e carrinho

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
+
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
@@ -14,6 +17,7 @@ pizzaJson.map((item, index) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -50,3 +54,82 @@ function fecharModal(){
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach( (item)=> {
     item.addEventListener('click', fecharModal);
 });
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1){
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach( (size)=> {
+    size.addEventListener('click', ()=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+    let key = cart.findIndex((item) => item.identifier == identifier);
+    if(key > -1){
+        cart[key].qt += modalQt;
+
+    } else{
+
+        cart.push({
+            identifier,
+            id:pizzaJson[modalKey].id,
+            size:size,
+            qt:modalQt
+        });
+    }
+    
+    upadteCart();
+    fecharModal();
+}); 
+
+function upadteCart(){
+    if(cart.length > 0){
+        c('aside').classList.add('show');
+        c('.cart').innerHTML ='';
+
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            let cartItem = c('.models .cart--item').cloneNode(true);
+            let pizzaSizeName;
+            switch(cart[i].size){
+                case 0:
+                    pizzaSizeName = 'P'
+                    break;
+                case 1:
+                    pizzaSizeName = 'M'
+                    break;
+                case 2:
+                    pizzaSizeName = 'G'
+                    break;
+                
+            }
+
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+
+            c('.cart').append(cartItem);
+            console.log(cartItem)
+
+
+        }
+
+    } else{
+        c('aside').classList.remove('show');
+
+    }
+}
